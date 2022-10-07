63cd7c0abd2c4ddeb8256e9e5a9f6e9d0118761b
ag076810 <88880369+ag076810@users.noreply.github.com>
2022-10-07 16:39:37 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,10 +1,10 @@
 export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
-  API_URL: "odp-rarity.vercel.app",
+  API_URL: "https://cof.mypinata.cloud/ipfs/QmeUYbtjWb84Y7bihdpLknj1ZPn2hFsdKu5riRxkTfBKkF/",
   COLLECTION_NAME: "LuckEggs",
   COLLECTION_TITLE: "LuckEggs",
-  COLLECTION_DESCRIPTION: '2000 "LuckEggs on ETHW.',
+  COLLECTION_DESCRIPTION: '2000 LuckEggs on ETHW.',
   COLLECTION_IMG_LINK: "https://cof.mypinata.cloud/ipfs/QmeUYbtjWb84Y7bihdpLknj1ZPn2hFsdKu5riRxkTfBKkF/3.png",
   CONTRACT: {
     // if you're not sure, set CONTRACT: {}, will remove opensea link
