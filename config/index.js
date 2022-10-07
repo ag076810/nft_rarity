716dcc1f1602716ccbcf76472138e9ebaa265a18
ag076810 <88880369+ag076810@users.noreply.github.com>
2022-10-07 16:33:42 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,14 +2,14 @@ export const config = {
   env: "prod",
   LOCAL_API_URL: "localhost:3000",
   API_URL: "odp-rarity.vercel.app",
-  COLLECTION_NAME: "OneDayPunks",
-  COLLECTION_TITLE: "One Day Punks",
-  COLLECTION_DESCRIPTION: '10k "One Day I\'ll Be A Punk"-punks.',
-  COLLECTION_IMG_LINK: "https://punkscape.xyz/og.png",
+  COLLECTION_NAME: "LuckEggs",
+  COLLECTION_TITLE: "LuckEggs",
+  COLLECTION_DESCRIPTION: '2000 "LuckEggs on ETHW.',
+  COLLECTION_IMG_LINK: "https://cof.mypinata.cloud/ipfs/QmeUYbtjWb84Y7bihdpLknj1ZPn2hFsdKu5riRxkTfBKkF/3.png",
   CONTRACT: {
     // if you're not sure, set CONTRACT: {}, will remove opensea link
-    CHAIN: "ethereum", //possible values of ethereum, matic (polygin), klatyn, solana
-    ADDRESS: "0x5537d90a4a2dc9d9b37bab49b490cf67d4c54e91", //note: for solana these values change, so currently not supported
+    CHAIN: "ethereumpow", //possible values of ethereum, matic (polygin), klatyn, solana
+    ADDRESS: "0x0b54d5619e32e33d6a42e918c7fa128a020365fe, //note: for solana these values change, so currently not supported
   },
   WEIGHTS: {},
 };
