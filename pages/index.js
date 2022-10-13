099ef236989cf9e8ca0b49a3bee1afcf05ae2158
ag076810 <88880369+ag076810@users.noreply.github.com>
2022-10-13 13:56:43 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -78,7 +78,7 @@ Home.getInitialProps = async ({ query }) => {
   return {
     title: config.COLLECTION_TITLE,
     description: config.COLLECTION_DESCRIPTION,
-    img: "./image/2.png",
+    img: config.COLLECTION_IMG_LINK,
     nfts,
     pages,
     filters,
